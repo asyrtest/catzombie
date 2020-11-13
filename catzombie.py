d3470a67f512ed05f8ef2dbacaf2b6f1ace4f19d
asyr <50852479+asyrattab@users.noreply.github.com>
2020-11-13 22:14:39 +0800

    Update catzombie.py
    fixed song at intro

--- catzombie.py
+++ catzombie.py
@@ -3,24 +3,23 @@ from pygame import mixer
 mixer.init()
 
 pygame.init()
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((1000, 500))
 done = False
+#song
+pygame.mixer.music.load("IntroSong.mp3")
+pygame.mixer.music.play(-1)
+
+#game logo and name      
+pygame.display.set_caption("CatZombie The Game")
+icon=pygame.image.load("cat-mask.png")
+pygame.display.set_icon(icon)
+
+#wallpaper
+image=pygame.image.load("grass.jpg").convert()
+screen.blit(image, (0, 0))
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    #song
-    pygame.mixer.music.load("IntroSong.mp3")
-    pygame.mixer.music.play(-1)
-    
-    #game logo and name      
-    pygame.display.set_caption("CatZombie The Game")
-    icon=pygame.image.load("cat-mask.png")
-    pygame.display.set_icon(icon)
-
-    #wallpaper
-    image=pygame.image.load("grass.jpg").convert()
-    screen.blit(image, (500, 500))
-    
     pygame.display.flip()
